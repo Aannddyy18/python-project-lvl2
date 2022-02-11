1bdf9ff161a02d669d8d4ff49727913ac0010943
Андрей Трушин <trand1@mail.ru>
2022-02-11 15:14:25 +0700

    first step lvl2

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import argparse
+import pathlib
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('source_file', metavar='first_file', type=open)
+    parser.add_argument('source_file', metavar='second_file', type=open)
+
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
