be199cdd0a72e9b767d63f48b5dafe268c37d237
Андрей Трушин <trand1@mail.ru>
2022-02-23 01:27:45 +0700

    working__on_stylish_step_6_lvl2

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -5,14 +5,17 @@ def stylish(values, replacer=' ', spaces_count=2, lvl=1, flag=''):
         result = '{\n'
         key_string = ''
         for key, value in values.items():
-            key_string = ''.join(key)
+            if key[0] == '+ ' or key[0] == '- ' or key[0] == '  ':
+                key_string = ''.join(key)
+            else:
+                key_string = '  ' + ''.join(key)
             result += f'{replacer * spaces_count * lvl}{key_string}: '
             if key_string.startswith(('+', '-')):
-                result += stylish(value, replacer, spaces_count, lvl + 3, flag='new') + '\n'
+                result += stylish(value, replacer, spaces_count, lvl + 2, flag='new') + '\n'
             else:
                 result += stylish(value, replacer, spaces_count, lvl + 2) + '\n'
-        if flag == 'new' and lvl > 3:
-            result += replacer * spaces_count * (lvl - 2) + '}'
+        if flag == 'new':
+            result += replacer * spaces_count * (lvl - 1) + '}'
         else:
             result += replacer * spaces_count * (lvl - 1) + '}'
     else:
