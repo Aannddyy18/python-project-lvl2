f53276d66e31178ed587dd33229f96a92ef52ffa
Андрей Трушин <trand1@mail.ru>
2022-03-01 00:43:56 +0700

    8_step_2lvl

---
+++ formatters/json_f.py
@@ -0,0 +1,20 @@
+"""Convert dictionary of difference to json format"""
+import json
+
+
+def jsonf(values, replacer=' ', spaces_count=2, lvl=1):
+    if isinstance(values, dict):
+        result = '{\n'
+        for key, value in values.items():
+            if key[0] == '+ ' or key[0] == '- ':
+                key_string = ''.join(key)
+            elif key[0] == '  ':
+                key_string = (''.join(key)).strip(' ')
+            else:
+                key_string = '' + ''.join(key)
+            result += f'{replacer * spaces_count * lvl}\"{key_string}\": '
+            result += jsonf(value, replacer, spaces_count, lvl + 1) + '\n'
+        result += replacer * spaces_count * (lvl - 1) + '}'
+    else:
+        result = f'\"{values}\"' if isinstance(values, str) else json.dumps(values)
+    return result
