e543c30563ebbdd8bccbdc26357fd51afbc1681e
Андрей Трушин <trand1@mail.ru>
2022-03-04 09:20:02 +0700

    WORKs_on_mistakes_lvl2

--- formatters/json_f.py
+++ formatters/json_f.py
@@ -6,12 +6,12 @@ def jsonf(values, replacer=' ', spaces_count=2, lvl=1):
     if isinstance(values, dict):
         result = '{\n'
         for key, value in values.items():
-            if key[0] == '+ ' or key[0] == '- ':
-                key_string = ''.join(key)
-            elif key[0] == '  ':
-                key_string = (''.join(key)).strip(' ')
+            if key[0] == '+ ':
+                key_string = ('+' + ''.join(key[1])).strip(' ')
+            elif key[0] == '- ':
+                key_string = ('-' + ''.join(key[1])).strip(' ')
             else:
-                key_string = '' + ''.join(key)
+                key_string = ('' + ''.join(key)).strip(' ')
             result += f'{replacer * spaces_count * lvl}\"{key_string}\": '
             result += jsonf(value, replacer, spaces_count, lvl + 1) + '\n'
         result += replacer * spaces_count * (lvl - 1) + '}'
