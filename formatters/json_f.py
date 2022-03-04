d821f8b81293f4a5f19c4acb2cb8603a01e6541c
Андрей Трушин <trand1@mail.ru>
2022-03-04 11:45:02 +0700

    WORKS_on_mistakes_lvl2

--- formatters/json_f.py
+++ formatters/json_f.py
@@ -4,6 +4,7 @@ import json
 
 def jsonf(values, replacer=' ', spaces_count=2, lvl=1):
     if isinstance(values, dict):
+        last = next(reversed(values))
         result = '{\n'
         for key, value in values.items():
             if key[0] == '+ ':
@@ -12,8 +13,15 @@ def jsonf(values, replacer=' ', spaces_count=2, lvl=1):
                 key_string = ('-' + ''.join(key[1])).strip(' ')
             else:
                 key_string = ('' + ''.join(key)).strip(' ')
-            result += f'{replacer * spaces_count * lvl}\"{key_string}\": '
-            result += jsonf(value, replacer, spaces_count, lvl + 1) + '\n'
+            if len(values) > 1:
+                result += f'{replacer * spaces_count * lvl}\"{key_string}\": '
+                if key == last:
+                    result += jsonf(value, replacer, spaces_count, lvl + 1) + '\n'
+                else:
+                    result += jsonf(value, replacer, spaces_count, lvl + 1) + ',' + '\n'
+            else:
+                result += f'{replacer * spaces_count * lvl}\"{key_string}\": '
+                result += jsonf(value, replacer, spaces_count, lvl + 1) + '\n'
         result += replacer * spaces_count * (lvl - 1) + '}'
     else:
         result = f'\"{values}\"' if isinstance(values, str) else json.dumps(values)
